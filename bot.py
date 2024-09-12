e97661f13e925f2e55ebc585c9f12633b819a1f6
dylanjian0 <dylanjian0@gmail.com>
2024-09-11 20:39:39 -0400

    initial commit; documentation in google doc

---
+++ bot.py
@@ -0,0 +1,11 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+botToken = os.getenv("botToken")
+bot.run(botToken)
