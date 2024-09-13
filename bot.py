83f2f52909fd360cc013ccdc45c876818a7d0c40
dylanjian0 <dylanjian0@gmail.com>
2024-09-13 04:31:42 -0400

    changes

---
+++ bot.py
@@ -0,0 +1,15 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+bot.run(DISCORD_BOT_TOKEN)
