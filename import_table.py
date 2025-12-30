e607216677b2eff81afcf8e6b3786ce9ee5794a8
Gmaster257 <therealgamemaster257@gmail.com>
2025-12-30 01:03:37 -0500

    Added edge case checks to import_table

--- import_table.py
+++ import_table.py
@@ -61,6 +61,14 @@ with open(sys.argv[1], 'r', encoding='utf-8') as csv_file:
     except KeyError:
         is_participant = True
 
+    # Verify that the participant form has all the attributes we need.
+    try:
+        if is_participant:
+            attributes.remove('Capstone Team')
+    except KeyError:
+        print('ERROR: CSV file missing required attributes. Check file contents and resubmit.')
+        sys.exit(2)
+
     # For each entry in the CSV file...
     for entry in reader:
         num_entries = num_entries + 1
@@ -104,7 +112,8 @@ with open(sys.argv[1], 'r', encoding='utf-8') as csv_file:
             is_duplicate = is_duplicate and old_data['last_name'] == entry['Last Name']
 
             # Check if the capstone team status is the same.
-            is_duplicate = is_duplicate and old_data['is_capstone'] == (entry['Capstone Team'] == 'Yes')
+            if is_participant:
+                is_duplicate = is_duplicate and old_data['is_capstone'] == (entry['Capstone Team'] == 'Yes')
 
             # Check if the roles are the same.
             is_duplicate = is_duplicate and set(records.get_user_roles(email)) == set(roles)
